d6930c5d90a2a15d116fb077a5e9493f1ad12063
bowang1993 <33351727+bowang1993@users.noreply.github.com>
2017-11-22 21:50:47 +0800

    Create Solution.java

---
+++ 206. Reverse Linked List/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        pre.next = null;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
