5864706529977e2126273022c2e0e3ab91c44753
bowang1993 <33351727+bowang1993@users.noreply.github.com>
2017-11-24 20:43:24 +0800

    Create Solution.java

---
+++ 389. Find the Difference/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char res=0;
+        for(int i=0;i<s.length();i++)
+            res ^= s.charAt(i);
+        for(int i=0;i<t.length();i++)
+            res ^= t.charAt(i);
+        return res;
+    }
+}
